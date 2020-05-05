d9f465ca4c01b0976e488bfc0efc8ebee8417568
chathuranga tennakoon <chathuranga@chathurangas-MacBook-Pro.local>
2020-05-05 16:04:01 +0900

    keycloak integration completed

--- src/main/java/com/springbootdev/examples/controller/ProductController.java
+++ src/main/java/com/springbootdev/examples/controller/ProductController.java
@@ -2,18 +2,30 @@ package com.springbootdev.examples.controller;
 
 import com.springbootdev.examples.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
 public class ProductController {
 
+    @RolesAllowed("ROLE_USER")
     @GetMapping("/products")
     public List<Product> getProducts() {
         return List.of(
                 Product.builder().id(1L).name("product 1").build(),
                 Product.builder().id(2L).name("product 2").build()
         );
+    }
+
+    @RolesAllowed("ROLE_ADMIN")
+    @PostMapping("/products")
+    public Product addProduct() {
+        return Product.builder()
+                .id(4L)
+                .name("new product")
+                .build();
     }
 }
---
+++ src/main/java/com/springbootdev/examples/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.springbootdev.examples.security;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        jsr250Enabled = true,
+        securedEnabled = true
+)
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected NullAuthenticatedSessionStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.cors().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+}
